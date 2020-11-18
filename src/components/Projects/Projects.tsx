f7eedecf9a2349e9777c88db907e69efc62e6bec
Artem Laboskin <laboss@list.ru>
2020-11-18 22:43:31 +0300

    Added Projects component

---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './Projects.scss';
+import SectionTitle from "../SectionTitle/SectionTitle";
+import ProjectCard from "../ProjectCard/ProjectCard";
+
+function Projects() {
+
+    return (
+        <section className="Projects" id="projects">
+            <SectionTitle count={3} name={'Latest Projects'} />
+            <ProjectCard
+                image="https://via.placeholder.com/1600x1000"
+                period="August - October 2020"
+                title="ezlingua"
+                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur consequatur enim inventore, ipsa modi nam numquam omnis ratione vero."
+                technologies={['Node.js', 'Express.js', 'Mongoose', 'React', 'Azure API']}
+                githubLink="/"
+                demoLink="/"
+                videoLink="/"
+            />
+            <ProjectCard
+                image="https://via.placeholder.com/1600x1000"
+                period="February - June 2020"
+                title="ezlingua-php"
+                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur consequatur enim inventore, ipsa modi nam numquam omnis ratione vero."
+                technologies={['PHP', 'Yii 2', 'MySQL', 'Azure API']}
+                githubLink="/"
+                demoLink="/"
+                videoLink="/"
+
+            />
+            <ProjectCard
+                image="https://via.placeholder.com/1600x1000"
+                period="September - December 2019"
+                title="solddr"
+                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur consequatur enim inventore, ipsa modi nam numquam omnis ratione vero."
+                technologies={['PHP', 'Yii 2', 'MySQL']}
+                githubLink="/"
+                demoLink="/"
+                videoLink="/"
+
+            />
+        </section>
+    );
+}
+
+export default Projects;
