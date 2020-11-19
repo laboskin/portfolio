622b781181d2fc9c7678a1bcd7cead440566b3ad
Artem Laboskin <laboss@list.ru>
2020-11-19 19:01:24 +0300

    Added AsideRightComponent

---
+++ src/components/AsideRight/AsideRight.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './AsideRight.scss';
+
+function AsideRight() {
+    return (
+        <aside className="AsideRight">
+            <a className="AsideRight-Text" href="mailto:joe.doe@gmail.com">
+                joe.doe@gmail.com
+            </a>
+        </aside>
+    );
+}
+
+export default AsideRight;
