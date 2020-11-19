bddae820aef496df271ff95a8e635a1ef2be4b63
Artem Laboskin <laboss@list.ru>
2020-11-20 02:59:30 +0300

    Added Loader component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.scss';
 import Header from "./components/Header/Header";
 import AsideLeft from "./components/AsideLeft/AsideLeft";
@@ -9,23 +9,29 @@ import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
+import Loader from "./components/Loader/Loader";
 
 function App() {
-  return (
-      <>
-          <Header />
-          <AsideLeft />
-          <AsideRight />
-          <main className="Main">
-              <Hero />
-              <About />
-              <Skills />
-              <Projects />
-              <Contact />
-          </main>
-          <Footer />
-      </>
-  );
+    const [loading, setLoading] = useState(true);
+
+    if (loading)
+        return <Loader completeHandler={():void => setLoading(false)} />
+
+    return (
+        <>
+            <Header />
+            <AsideLeft />
+            <AsideRight />
+            <main className="Main">
+                <Hero />
+                <About />
+                <Skills />
+                <Projects />
+                <Contact />
+            </main>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
