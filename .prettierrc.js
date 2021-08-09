310db7155e7bf5efb07f4c7e6baa96ba0422bab0
Artem Laboskin <laboss@list.ru>
2021-08-09 14:47:19 +0300

    Added prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    trailingComma: 'all',
+    singleQuote: true,
+    tabWidth: 2,
+    useTabs: false,
+    printWidth: 100,
+    jsxSingleQuote: false,
+    jsxBracketSameLine: false,
+};
