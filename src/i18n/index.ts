d7ef1283da556261f6088e98c2386351de1db200
Artem Laboskin <laboss@list.ru>
2021-08-09 14:49:11 +0300

    Improved i18next config

---
+++ src/i18n/index.ts
@@ -0,0 +1,36 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+import { en, ru } from './locales';
+
+const resources = {
+  en: {
+    translation: en,
+  },
+  ru: {
+    translation: ru,
+  },
+};
+
+i18n.on('languageChanged', () => {
+  document.title = i18n.t('documentTitle');
+  document.documentElement.lang = i18n.language;
+});
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    debug: false,
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ['localStorage', 'navigator', 'htmlTag'],
+      lookupLocalStorage: 'i18nextLng',
+      caches: ['localStorage'],
+    },
+  });
+
+export default i18n;
---
+++ src/i18n/locales/en.ts
@@ -0,0 +1,81 @@
+export const en = {
+  documentTitle: 'Artem Laboskin',
+
+  header: {
+    nav: {
+      about: 'About',
+      skills: 'Skills',
+      projects: 'Projects',
+      contact: 'Contact',
+    },
+    language: {
+      code: 'ru',
+      name: 'Русский',
+    },
+  },
+  hero: {
+    prefix: 'Hi, my name is',
+    primaryHeading: 'Artem Laboskin.',
+    secondaryHeading: 'I am a front-end developer.',
+    description:
+      'I specialize in building web applications, that not only have high performance and clean code, but also provide an exceptional user experience.',
+    button: 'Contact me',
+  },
+  about: {
+    title: 'About Me',
+    text: {
+      first: "Hi! I'm Artem Laboskin, a front-end developer based in <0>Kazan, Russia</0>.",
+      second:
+        "Since I was a child I've been a big fan of information technologies and programming. " +
+        'I enjoy creating software that solves real-world problems and makes this world a better place. ' +
+        'As a developer, I strive to always create web applications that have efficient, clean and scalable code and provide the best user experience.',
+      third:
+        "In June 2020 I graduated from <0>Kazan State University</0> with a bachelor's degree in Computer Science and started learning JavaScript, React and many other technologies and tools in order to build a successful career as a front-end developer.",
+      fourth:
+        'Besides programming my biggest passion is traveling and exploring the world. My experience of living and working in the US for half a year in 2019 made it easy for me to communicate and work together with people all around the world.',
+    },
+    photoAlt: 'My photo',
+  },
+  skills: {
+    title: 'My Skills',
+    frontend: 'Front-end',
+    backend: 'Back-end',
+    other: 'Other',
+    some: 'Some',
+    basics: 'basics',
+  },
+  projects: {
+    title: 'Recent Projects',
+    descriptions: {
+      portfolio:
+        'A small serverless React application. Online version of my resume/portfolio that you are currently looking through.',
+      ezlingua:
+        'A web application for learning foreign languages through reading short stories and completing various vocabulary trainings from vocabulary cards to word constructor.',
+      'ezlingua-php':
+        "A legacy version of ezlingua project listed above. It was built as a Bachelor's thesis at the university and later used as a prototype for a brand new React/NodeJS application.",
+      soldr:
+        'A website for viewing and posting local advertisements with sections devoted for housing, vehicles, items and services.',
+    },
+  },
+  contact: {
+    overline: "What's Next?",
+    title: 'Contact me',
+    description:
+      'I am currently looking for a job. I would consider all opportunities, including remote job or relocation to another city or country. Feel free to ask any questions, I would be pleased to answer them.',
+    button: 'Send a message',
+  },
+  months: {
+    january: 'January',
+    february: 'February',
+    march: 'March',
+    april: 'April',
+    may: 'May',
+    june: 'June',
+    july: 'July',
+    august: 'August',
+    september: 'September',
+    october: 'October',
+    november: 'November',
+    december: 'December',
+  },
+};
