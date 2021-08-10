a9abd06a46d58c9be48f22ff61de9073de9699c7
Artem Laboskin <laboss@list.ru>
2021-08-10 12:37:21 +0300

    Added GlobalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,35 @@
 import React, { useState } from 'react';
-import './App.scss';
 import { AsideLeft, AsideRight, Footer, Header, Loader, Main } from './components';
+import { GlobalStyles } from './globalStyles';
 import { About, Contact, Hero, Projects, Skills } from './pages';
 
-function App() {
+export const App: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+
   if (document.location.pathname !== '/') {
     document.location.pathname = '/';
   }
 
-  const [loading, setLoading] = useState(true);
-  if (loading) return <Loader completeHandler={(): void => setLoading(false)} />;
-
   return (
     <>
-      <Header />
-      <AsideLeft />
-      <AsideRight />
-      <Main>
-        <Hero />
-        <About />
-        <Skills />
-        <Projects />
-        <Contact />
-      </Main>
-      <Footer />
+      {loading ? (
+        <Loader completeHandler={(): void => setLoading(false)} />
+      ) : (
+        <>
+          <Header />
+          <AsideLeft />
+          <AsideRight />
+          <Main>
+            <Hero />
+            <About />
+            <Skills />
+            <Projects />
+            <Contact />
+          </Main>
+          <Footer />
+        </>
+      )}
+      <GlobalStyles />
     </>
   );
-}
-
-export default App;
+};
